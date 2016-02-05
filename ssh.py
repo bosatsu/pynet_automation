0102dbeb0069ca5a613b03a9a59400727f2e2897
bosatsu <x.infinity.x@gmail.com>
2016-02-04 19:47:18 -0700

    Adds files

---
+++ ssh.py
@@ -0,0 +1,43 @@
+import paramiko
+import time
+from getpass import getpass
+
+def disable_paging(session, command="terminal length 0\n", delay=1):
+
+	session.send("\n")
+	session.send(command)
+
+	# Wait for the command to complete  
+	time.sleep(delay)
+
+	output = session.recv(65535)
+	return output
+
+def main()
+	ip_addr = "50.76.53.27"
+	username = "pyclass"
+	password = getpass()
+	port = 8022
+	
+	session_init = paramiko.SSHClient()
+	session_init.set_missing_host_key_policy(
+		paramiko.AutoAddPolicy())
+
+	session_init.connect(ip, username=username, password=password, port=port)
+	session = session_init.invoke_shell()
+
+	output = disable_paging(session)
+
+	session.send("\n")
+	session.send("show version\n")
+
+	# Wait for the command to complete  
+	time.sleep(5)
+
+	output = session.recv(65535)
+	print output
+
+	session_init.close()
+
+if __name__ == "__main__":
+	main()
